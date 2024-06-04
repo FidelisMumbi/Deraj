64e75126842a600eaef9fb813259dc1fa5d77ce7
FidelisMumbi <fidelismumbi111@gmail.com>
2024-06-04 06:53:52 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 function show_services(elementId) {
     var element = document.getElementById(elementId);
-    if (element.style.display === 'none' || element.style.display === '') {
+    if (element.style.display == 'none') {
         element.style.display = 'block';
     } else {
         element.style.display = 'none';
